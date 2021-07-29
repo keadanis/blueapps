f2445c2235fac16f2ed4726b8ef40f5464a9b758
blue-connect <contato@blueconnect.com.br>
2021-07-29 16:14:01 +0000

    Inclusão de app

--- business_requirement_crm/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2019 Tecnativa - Victor M.M. Torres
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import models
-from . import wizard
--- business_requirement_deliverable/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2016-2019 Elico Corp (https://www.elico-corp.com).
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import business
-from . import res_partner
-from . import brd_section
